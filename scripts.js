16d37bff9561d48aa2c00af6d71bb0e7600a627d
joaosusaki <jrabalem@gmail.com>
2022-06-09 18:25:53 -0300

    2 commit

--- scripts.js
+++ scripts.js
@@ -100,5 +100,77 @@ fetch("./content.json")
 
   //#endregion
 
+    //#region SECTION 3
+    document
+    .getElementById('section3-titulo1-span1')
+    .textContent = data.section3.parte1.titulo.span1
+    document
+    .getElementById('section3-titulo1-span2')
+    .textContent = data.section3.parte1.titulo.span2
 
+    document
+    .getElementById('section3-texto1-span1')
+    .textContent = data.section3.parte1.texto.span1
+    document
+    .getElementById('section3-texto1-span2')
+    .textContent = data.section3.parte1.texto.span2
+
+    document
+    .getElementById('section3-titulo2-span1')
+    .textContent = data.section3.parte2.titulo.span1
+    document
+    .getElementById('section3-titulo2-span2')
+    .textContent = data.section3.parte2.titulo.span2
+
+    document
+    .getElementById('section3-texto2-span1')
+    .textContent = data.section3.parte2.texto.span1
+    document
+    .getElementById('section3-texto2-span2')
+    .textContent = data.section3.parte2.texto.span2
+    document
+    .getElementById('section3-texto2-span3')
+    .textContent = data.section3.parte2.texto.span3
+    document
+    .getElementById('section3-texto2-span4')
+    .textContent = data.section3.parte2.texto.span4
+    document
+    .getElementById('section3-texto2-span5')
+    .textContent = data.section3.parte2.texto.span5
+
+    document
+    .getElementById('section3-botao')
+    .textContent = data.section3.botao
+    //#endregion
+
+    //#region Section4
+    document
+    .getElementById('section4-titulo')
+    .textContent = data.section4.titulo
+
+    document
+    .getElementById('section4-botao')
+    .textContent = data.section4.botao
+    //#endregion
+
+    //#region SECTION 5
+    document
+    .getElementById('section5-titulo-span1')
+    .textContent = data.section5.titulo.span1
+    document
+    .getElementById('section5-titulo-span2')
+    .textContent = data.section5.titulo.strong
+    document
+    .getElementById('section5-subtitulo')
+    .textContent = data.section5.subtitulo
+    document
+    .getElementById('section5-texto-span1')
+    .textContent = data.section5.texto.span1
+    document
+    .getElementById('section5-texto-span2')
+    .textContent = data.section5.texto.span2
+    document
+    .getElementById('section5-botao')
+    .textContent = data.section5.botao
+    //#endregion
 });
